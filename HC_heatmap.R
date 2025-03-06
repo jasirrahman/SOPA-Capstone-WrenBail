84e98007b47ca3743bff19a21ded52bc3cb0b93c
jasirrahman <51933869+jasirrahman@users.noreply.github.com>
2025-03-06 11:58:22 -0600

    Update HC_heatmap.R
    - Fixed loading + plotting of HC ZIP code data
    - Plotted heat map for zip code arrest frequency
    - Plotted heat map for zip code mean bail amount

--- HC_heatmap.R
+++ HC_heatmap.R
@@ -11,12 +11,16 @@ library(tidyr)
 library(tigris)
 library(sf)
 library(ggplot2)
-# Load data
+
+## Load data
 # Note: When running make sure that path for file is specific to your computer
 load("/Users/jasirrahman/Desktop/SOPA Capstone/R/Data/Harris_County_Cleaned_v2.RData")
 
+# Download shape file here: https://www2.census.gov/geo/tiger/TIGER2023/FACES/tl_2023_48201_faces.zip
 # Set path to the extracted shapefile
 shapefile_path <- "/Users/jasirrahman/Desktop/SOPA Capstone/R/Visualization/tl_2023_48201_faces/tl_2023_48201_faces.shp"
+
+##### Data Preparation #####
 
 # Read shapefile using sf
 harris_faces <- st_read(shapefile_path)
@@ -24,44 +28,55 @@ harris_faces <- st_read(shapefile_path)
 # Print basic info
 print(harris_faces)
 
+# Aggregate census blocks into ZIP code areas
+harris_zip <- harris_faces %>%
+  group_by(ZCTA5CE20) %>%
+  summarise(geometry = st_union(geometry), .groups = "drop")
+
+# Check if ZIP codes plot correctly
+# Plot the ZIP code areas
 ggplot() +
-  geom_sf(data = harris_faces, fill = "lightgray", color = "black", size = 0.3) +
+  geom_sf(data = harris_zip, fill = NA, color = "blue") +
   theme_void() +
-  labs(title = "Harris County Faces (Census Blocks)")
+  ggtitle("ZIP Code Tabulation Areas in Harris County")
 
-options(tigris_use_cache = TRUE)
+###### Plot ZIP Code counts ######
 
-# Use the correct FIPS code for Harris County, TX (48201)
-harris_zips_tigris <- zctas(state = "48", county = "201", year = 2010)
+# Create new df, group the bail data by ZIP code
+bailZIPCounts <- bail %>% 
+  group_by(defZIP) %>% 
+  summarize(count = n())    # Aggregate counts
 
-# Print summary
-print(harris_zips_tigris)
+# Rename collumns for merging
+bailZIPCounts <- rename(bailZIPCounts, ZCTA5CE20 = defZIP)
 
-# Plot roads
+# Merge data for heatmap plot
+harris_zip_final <- inner_join(harris_zip, bailZIPCounts, by = join_by(ZCTA5CE20))
+
+# Plot Arrest Heatmap
 ggplot() +
-  geom_sf(data = harris_zips_tigris, color = "red", size = 0.2) +
-  theme_minimal() +
-  labs(title = "Harris County ZIP Codes (TIGER Data)")
+  geom_sf(data = harris_zip_final, aes(fill = count), color = "white", alpha = 0.8) +
+  (scale_fill_viridis_c(option = "plasma", direction = -1)) + 
+  theme_void() +
+  ggtitle("Heatmap of Arrests by ZIP Code in Harris County") +
+  labs(fill = "Observation Count")
 
-#### Chat
+###### Plot ZIP Code Bail Amount Data ######
+# Create new df, group the bail data by ZIP code
+bailZIPAmounts <- bail %>% 
+  group_by(defZIP) %>% 
+  summarize(bond = mean(bondAmt))   # Aggregate counts
 
-# Get Texas ZCTAs for 2010 (the latest year available for ZCTAs in tigris)
-texas_zctas <- zctas(state = "TX", year = 2010)
+# Rename collumns for merging
+bailZIPAmounts <- rename(bailZIPAmounts, ZCTA5CE20 = defZIP)
 
-# Print the structure of the dataset
-print(texas_zctas)
+# Merge data for heatmap plot
+harris_zip_bail_final <- inner_join(harris_zip, bailZIPAmounts, by = join_by(ZCTA5CE20))
 
-harris_boundary <- counties(state = "TX", year = 2023) %>%
-  filter(NAME == "Harris")
-
-harris_zips <- st_intersection(texas_zctas, harris_boundary)
-
-# Print summary
-print(harris_zips)
-
+# Plot Arrest Heatmap
 ggplot() +
-  geom_sf(data = harris_zips, fill = "lightblue", color = "black", size = 0.3) +
-  theme_minimal() +
-  labs(title = "Harris County ZIP Code Tabulation Areas (ZCTA5)",
-       subtitle = "Filtered from Texas ZCTAs (2010 TIGER Data)",
-       caption = "Source: U.S. Census Bureau via tigris")
+  geom_sf(data = harris_zip_bail_final, aes(fill = bond), color = "white", alpha = 0.8) +
+  (scale_fill_viridis_c(option = "plasma", direction = -1)) + 
+  theme_void() +
+  ggtitle("Mean Bail Amounts are Higher \nin South and Northeast Houston") +
+  labs(fill = "Mean Bond Amount")
